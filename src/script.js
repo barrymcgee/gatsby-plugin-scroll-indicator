0bb1c50bb2481fcab7a774119a422cd3623d8d72
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-02-18 23:22:09 +0000

    Add first pass at scroll indicator

---
+++ src/script.js
@@ -0,0 +1,32 @@
+(function() {
+  // Reference: http://www.html5rocks.com/en/tutorials/speed/animations/
+  let scrolling = false;
+  const indicator = document.getElementById('indicator');
+
+  const getPercentage = (currentPos, totalScroll) => {
+    return Math.floor((currentPos / totalScroll) * 100);
+  };
+
+  window.addEventListener('scroll', function() {
+    let currentPos = window.scrollY,
+      innerHeight = window.innerHeight,
+      // https://javascript.info/size-and-scroll-window#width-height-of-the-document
+      scrollHeight = Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+      ),
+      scrollDistance = scrollHeight - innerHeight;
+    if (!scrolling) {
+      window.requestAnimationFrame(function() {
+        let indicatorWidth = getPercentage(currentPos, scrollDistance);
+        indicator.setAttribute('style', `width: ${indicatorWidth}%`);
+        scrolling = false;
+      });
+      scrolling = true;
+    }
+  });
+})();

a3d4afe4517ed45766b9a506939f62acadc4d286
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-05-31 21:03:43 +0100

    Separate enableScroller and indicatorPresent conditonals

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -25,49 +25,51 @@ exports.onClientEntry = (a, pluginOptions = {}) => {
     // check if viewport already has a scroll indicator
     const indicatorPresent = document.querySelector(`#${node.id}`);
 
-    if (enableScroller && !indicatorPresent) {
-      document.body.appendChild(node);
-      let scrolling = false;
-      const indicator = document.getElementById(node.id);
-      // Determine width of scroll indicator
-      const getIndicatorPercentageWidth = (currentPos, totalScroll) => {
-        return Math.floor((currentPos / totalScroll) * 100);
-      };
-      // Find the total height of scrolling window
-      const getScrollHeight = () => {
-        // https://javascript.info/size-and-scroll-window#width-height-of-the-document
-        return Math.max(
-          document.body.scrollHeight,
-          document.documentElement.scrollHeight,
-          document.body.offsetHeight,
-          document.documentElement.offsetHeight,
-          document.body.clientHeight,
-          document.documentElement.clientHeight
-        );
-      };
-      // Add throttled listener to update on scroll
-      window.addEventListener('scroll', function() {
-        let currentPos = window.scrollY,
-          innerHeight = window.innerHeight,
-          scrollHeight = getScrollHeight(),
-          scrollDistance = scrollHeight - innerHeight;
-        if (!scrolling) {
-          window.requestAnimationFrame(function() {
-            let indicatorWidth = getIndicatorPercentageWidth(
-              currentPos,
-              scrollDistance
-            );
-            indicator.setAttribute(
-              'style',
-              `width: ${indicatorWidth}%;position: fixed;height: 3px;background-color: ${
-                options.color
-              };top: 0;left: 0;transition:width 0.25s`
-            );
-            scrolling = false;
-          });
-          scrolling = true;
-        }
-      });
+    if (enableScroller) {
+      if (!indicatorPresent) {
+        document.body.appendChild(node);
+        let scrolling = false;
+        const indicator = document.getElementById(node.id);
+        // Determine width of scroll indicator
+        const getIndicatorPercentageWidth = (currentPos, totalScroll) => {
+          return Math.floor((currentPos / totalScroll) * 100);
+        };
+        // Find the total height of scrolling window
+        const getScrollHeight = () => {
+          // https://javascript.info/size-and-scroll-window#width-height-of-the-document
+          return Math.max(
+            document.body.scrollHeight,
+            document.documentElement.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.offsetHeight,
+            document.body.clientHeight,
+            document.documentElement.clientHeight
+          );
+        };
+        // Add throttled listener to update on scroll
+        window.addEventListener('scroll', function() {
+          let currentPos = window.scrollY,
+            innerHeight = window.innerHeight,
+            scrollHeight = getScrollHeight(),
+            scrollDistance = scrollHeight - innerHeight;
+          if (!scrolling) {
+            window.requestAnimationFrame(function() {
+              let indicatorWidth = getIndicatorPercentageWidth(
+                currentPos,
+                scrollDistance
+              );
+              indicator.setAttribute(
+                'style',
+                `width: ${indicatorWidth}%;position: fixed;height: 3px;background-color: ${
+                  options.color
+                };top: 0;left: 0;transition:width 0.25s`
+              );
+              scrolling = false;
+            });
+            scrolling = true;
+          }
+        });
+      }
     } else {
       // Try to assign scrollIndicator if it is already attached to the DOM
       const scrollIndicator = document.querySelector(
